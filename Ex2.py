419fa724256900f2d8094de93642c7268c6406d7
Gerenko <student_onu@ukr.net>
2020-09-04 08:32:06 +0300

    first commit

---
+++ Ex2.py
@@ -0,0 +1,8 @@
+#Написать программу, которая выводит на экран таблицу Esc-посл.
+print('\\a    Bell(alert)')
+print('\\b    Backspace')
+print('\\n    new line')
+print('\\t    Horizontal tab')
+print('\\\\   Back slash \\')
+print('\\"    Double quatation mark \"')
+print('\\\'    Single quatation mark \'')
