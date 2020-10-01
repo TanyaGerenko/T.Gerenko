a43e6f361979ed24bc0e2ac39d771e66c4480a22
TanyaGerenko <tanya.hillel.2020@ukr.net>
2020-10-01 22:18:27 +0300

    25

---
+++ Ex25.py
@@ -0,0 +1,19 @@
+books=[
+[34587,'Learning Python,Mark Lutz',4,40.95],
+[98762,'Programming Python,Mark Lutz',5,56.80],
+[77226,'Head First Python,Paul Barry',3,32.95],
+[88112,'Einfuhrung in Python3,Bernd Klein',3,24.99]
+    ]
+
+
+def myfunc(*args):
+    
+    for arg in args:
+        c=arg[2]*arg[3]
+        if c<100:
+            c=c+10
+    return (arg[0],c)
+
+books_zakaz = tuple(map(lambda x: myfunc(x),books))
+
+print(books_zakaz)
